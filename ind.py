62b0b0a109f0f26d4ea84c15e32890c30666f130
ShveczovaKS <78951519+ShveczovaKS@users.noreply.github.com>
2021-11-30 00:15:54 +0300

    Add files via upload

---
+++ ind.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def summy(*args):
+    if args:
+        summa = 0
+        numma = 0
+
+        values = [float(arg) for arg in args]
+
+        for inx in values:
+            if inx < 1:
+                numma += 1
+            if numma == 1 and inx > 0:
+                summa += inx
+            if numma == 2:
+                print(summa)
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print(summy(-1, 100, 2, 3, -1))
+    print(summy(15, 90, -14, 15, 15, 15, -1))
